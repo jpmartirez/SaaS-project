849580285375b34164951e67577810d4f924b63a
jpmartirez <johnpaulmartirez0915@gmail.com>
2026-03-21 13:16:14 +0800

    add recent session table and make it responsive to mobile devices

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import CompanionCard from "@/components/CompanionCard";
 import CompanionList from "@/components/CompanionList";
 import CTA from "@/components/CTA";
 import { Button } from "@/components/ui/button";
+import { recentSessions } from "@/constants";
 import React from "react";
 
 const Page = () => {
@@ -9,13 +10,38 @@ const Page = () => {
     <main>
       <h1 className="">Popular Companions</h1>
       <section className="home-section">
-        <CompanionCard id="123" name="Neura the Brainy Explorer" topic="Neural Network of the Brain" subject="Science" duration={45} color="#ffda6e"/>
-        <CompanionCard id="456" name="Countsy the Number Wizard" topic="Derivatives and Integral" subject="Mathematics" duration={30} color="#e5d0ff"/>
-        <CompanionCard id="789" name="verba the Vocabulary Builder" topic="English Literature" subject="Language" duration={30} color="#bde7ff"/>
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="Science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives and Integral"
+          subject="Mathematics"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="Language"
+          duration={30}
+          color="#bde7ff"
+        />
       </section>
 
       <section className="home-section">
-        <CompanionList />
+        <CompanionList
+          title="Recently Completed Sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
         <CTA />
       </section>
     </main>
