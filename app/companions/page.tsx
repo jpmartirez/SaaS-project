e7d757cb978db35f5cb42c20b0da7c316d0d9902
jpmartirez <johnpaulmartirez0915@gmail.com>
2026-03-29 20:18:24 +0800

    create the companion library and add search and input components

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,7 +1,40 @@
+import CompanionCard from "@/components/CompanionCard";
+import SearchFilter from "@/components/SearchFilter";
+import SearchInput from "@/components/SearchInput";
+import { getAllCompanions } from "@/lib/actions/companion.action";
+import { getSubjectColor } from "@/lib/utils";
 import React from "react";
 
-const CompanionsLibrary = () => {
-  return <div>CompanionsLibrary</div>;
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+
+  const subject = filters.subject ? filters.subject : "";
+  const topic = filters.topic ? filters.topic : "";
+
+  const companions = await getAllCompanions({ subject, topic });
+
+  console.log(companions);
+
+  return (
+    <main>
+      <section className="flex justify-between gap-4 max-sm:flex-col ">
+        <h1>Companion Library</h1>
+        <div className="flex gap-4">
+          <SearchInput />
+          <SearchFilter />
+        </div>
+      </section>
+      <section className="companions-grid ">
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
+      </section>
+    </main>
+  );
 };
 
 export default CompanionsLibrary;
